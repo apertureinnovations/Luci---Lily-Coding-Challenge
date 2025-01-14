51091158da06eee6cc0749e7ee640e712e39bf3c
apertureinnovations <jakepalmer42@hotmail.com>
2025-01-14 14:25:40 +0000

    New initial branch with classes

---
+++ main.py
@@ -0,0 +1,17 @@
+
+class player:
+
+    def __init__(self):
+        self.name = ""
+
+
+class board:
+
+    def __init__(self):
+        self.board = [[" " for i in range(20)] for i in range(20)]
+
+class pieces:
+
+    def __init__(self):
+        pass
+
